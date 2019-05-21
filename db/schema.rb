fc5dbbb915c867b311041520f8819acc3f74e61e
marita30 <marita.jose.ch@gmail.com>
2019-05-21 12:19:24 -0600

    creando una estructura de trabajo para el posts , con el siguiente comando rails generate scaffold post name:string title:string content: text y luego rake db:migrate...... se crea un model , controller y una vista , con su db y sus test....

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_212311) do
+ActiveRecord::Schema.define(version: 2019_05_21_172002) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
