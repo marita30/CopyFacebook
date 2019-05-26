f09f1b5461d42602e783cc56a346d9c0184c53f1
Karel <edmolina4@gmail.com>
2019-05-25 22:41:08 -0600

    trying set models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts
 
+  attr_accessor :name, :email, :remember_me, :profile_attributes
+  has_one :profile
+  accepts_nested_attributes_for :profile
 
   # has_many :posts, dependent: :destroy
   # has_many :likes, dependent: :destroy
