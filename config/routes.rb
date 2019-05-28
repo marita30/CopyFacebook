1d49227ed5913171f530aff8aedf3af72c4af5d4
marita30 <marita.jose.ch@gmail.com>
2019-05-28 16:47:31 -0600

    creando un nuevo model, contructor y view para comments y tambien una nueva migrate , scss y test.... editanto el archivo post.rb poniendo has__many :comments de la relacionde muchos , editando el archivo show.html.erb agregandole el comments, editando el archivo routes escribiendo resources :comments y luego rake db:migrate para que corra las prueas.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-  resources :posts
+  resources :posts do
+    resources :comments
+end
   root 'posts#index'
 
   get "/users/:id", to: "users#show"
