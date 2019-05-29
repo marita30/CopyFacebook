1c9c9db4f9c1b5995e923d1558cfc99a512947fc
marita30 <marita.jose.ch@gmail.com>
2019-05-29 11:31:31 -0600

    editando los archivos comments_controller.rb, posts_controller.rb, comment.rb, user.rb, show.html.erb , en el archivo commentes_controller pusimos al usuario , porque solo estaba el post , en el archivo posts_controller en el metodo show le pasamos @post en el comments.rb agregamos otro belongs_to :user y en el user.rb le agregamos la relacion de muchos has-many :comments y volvimos update a la base dato con el siguiente codigo rake db:drop db:cretae db:migration

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,14 @@
+require 'pry'
+
 class CommentsController < ApplicationController
 
   def create
+    binding.pry
     @post = Post.find(params[:post_id])
+    @user = current_user.id
     @comment = @post.comments.create(params[:comment].permit(:name, :body))
+    @comment.user_id = @user
+    @comment.save
     redirect_to post_path(@post)
   end
 end
